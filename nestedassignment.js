da6b3903d26038aaf3853a4eafa54a8c748e9063
Sharonoito <oitosharonakoth@gmail.com>
2021-03-19 22:39:03 +0300

    first commit

---
+++ nestedassignment.js
@@ -0,0 +1,15 @@
+function name() {    
+    var a="Shadya"
+    var b="Pamera"
+    function friends() {
+           var c=a  + " and" +" " +  b +  " " + "are friends";
+           console.log(c);
+               
+       }
+       friends()
+   }
+   
+   
+       name();
+   
+   
